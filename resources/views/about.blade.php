66358bc564b028022731c006859014daa9d1f133
noosabaktee <ramanusabakti03@gmail.com>
2025-08-30 21:54:31 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+    <a href="{{route('contact')}}">Halaman Kontak</a>
+    <a href="{{route('user','Rama')}}">Halaman user Rama</a>
+    <a href="{{route('edit')}}">Halaman edit</a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit enim omnis tempore iure dolores saepe quasi. Quibusdam natus, id ducimus culpa vero numquam. Saepe aliquid, repudiandae in autem aspernatur vel.</p>
+</body>
+</html>
